2779dcd91c79bbeb90cd38fa5f6dc1b1904ccbce
Jussi Jokinen <jussijokinen@iki.fi>
2010-11-16 22:30:46 +0200

    Changed licence to MIT

--- centage/js/centage.js
+++ centage/js/centage.js
@@ -1,17 +1,6 @@
-/* This file is part of Centage!.
-* Copyright (C) Jussi Jokinen <jussijokinen@iki.fi>.
-* Centage! is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Centage! is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Centage!.  If not, see <http://www.gnu.org/licenses/>. */
+/* This file is part of Centage!. */
+/* Copyright (C) 2010 Jussi Jokinen <jussijokinen@iki.fi>. */
+/* Licenced under MIT licence */
 
 function createGrid(columns) {
 	var grid = '<div id="grid_wrapper"><div id="grid">';
